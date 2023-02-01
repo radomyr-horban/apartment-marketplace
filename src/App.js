9e08ea7b35d3a4e58a7a647c1e8529c2cf9b81d2
Radomyr Horban <horban.radomyr@gmail.com>
2023-02-01 08:44:30 +0200

    created components templates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-
 import './App.css';
+import Apartment from './components/Apartment';
+import ApartmentForm from './components/ApartmentForm';
+import SortPanel from './components/SortPanel';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Apartments Marketplace</h1>
+      <ApartmentForm />
+      <SortPanel />
+
+      <Apartment />
+      <Apartment />
+      <Apartment />
     </div>
   );
 }
